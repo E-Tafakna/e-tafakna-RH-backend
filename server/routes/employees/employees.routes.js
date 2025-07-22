14e053a4b749725db889227757e30a2583676868
wael-ben-salem <108833476+wael-ben-salem@users.noreply.github.com>
2025-07-22 18:15:08 +0100

    Update employees.routes.js

--- server/routes/employees/employees.routes.js
+++ server/routes/employees/employees.routes.js
@@ -7,6 +7,7 @@ const {
   deleteEmployee,
   getManagers,
   getEmployeeByCode,
+  checkEmployeeCodeExists,
   getCEOs,
   getEmployeeHierarchy
 } = require('../../controllers/employees/employeesController');
@@ -16,11 +17,13 @@ const router = express.Router();
 router.get('/', getAllEmployees);
 router.get('/ceos', getCEOs); 
 router.get('/hierarchy', getEmployeeHierarchy); 
-router.get('/managers', getManagers);          
+router.get('/managers', getManagers); 
+router.get('/check-code', checkEmployeeCodeExists);
 router.get('/:id', getEmployeeById);
 router.post('/', createEmployee);
 router.put('/:id', updateEmployee);
 router.get('/code/:code_employe', getEmployeeByCode);
 router.delete('/:id', deleteEmployee);
+
 
 module.exports = router;
